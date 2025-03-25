c6f211b8cfca5b537edbdaf5c997bbe999b1228e
rkritzar54 <rkritzar9@gmail.com>
2025-03-25 13:33:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 const hamburger = document.getElementById('hamburger');
 const mobileMenu = document.querySelector('nav ul.mobile-menu');
 
-// Toggle the menu on click
+// Toggle mobile menu open/close
 hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
 });
 
-// Close the menu when a menu item is clicked
-mobileMenu.addEventListener('click', () => {
-    mobileMenu.classList.remove('active');
-});
-
-// Close the menu if the user clicks outside of it
+// Close mobile menu when clicking outside
 window.addEventListener('click', (event) => {
     if (!hamburger.contains(event.target) && !mobileMenu.contains(event.target)) {
         mobileMenu.classList.remove('active');
